6b6ca9124b0d39b8bbaa9fe634c8d3493d4ded3d
Hanz Cocchi Guerrero <hanzcg@hotmail.com>
2025-04-17 19:17:12 -0500

    Update Member.cs

--- CADN.Domain/Member.cs
+++ CADN.Domain/Member.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Last_Name { get; set; }
         public string Type { get; set; }
         public string Address { get; set; }
     }
